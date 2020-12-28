876bbec3378fb9afdb2e07f578262189e3faafa4
DenerThiago21 <denerthi.ibi@gmail.com>
2020-12-28 15:59:12 -0300

    Login Page

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,25 @@
+import React, {Component} from  'react';
+
+import ait from '../../assets/AIT.png'
+
+import './styles.css';
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="login text-center">
+                <form className="form-signin">
+                    <img className="mb-4 rounded-circle" src={ait} alt="" width="80" height="80" />
+                    <h1 className="h3 mb-3 font-weight-normal">Faça o Login</h1>
+                    <label for="inputUser" className="sr-only">Usuário</label>
+                    <input type="user" id="inputUser" className="form-control" placeholder="usuário" required autoFocus />  
+                    <label for="inputPassword" className="sr-only">Senha</label>
+                    <input type="password" for="inputPassword" className="form-control" placeholder="Senha" required />
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Evolution from './pages/Evolution';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <Switch>
-                <Route path='/' exact component={Home} />
+                <Route path='/' exact component={Login} />
                 <Route path='/home' exact component={Home} />
                 <Route path='/evolution' component={Evolution} />
             </Switch>
